7ecd807e4202eb07d0d400c67fcabbcbabdc1313
Marat Alksanyan <marataleks@gmail.com>
2021-08-01 16:44:07 +0500

    Changed not found page style and table cells styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
 
 	public coinsInfo = new MatTableDataSource<any[]>();
 	private getConfigUrl: string = '/web/v1/cryptocurrency/listings/latest?start=1&limit=30';
-	displayedColumns: string[] = ['name', 'symbol', 'slug', 'price', 'percent_change_1h', 'percent_change_24h', 'market_cap', 'last_updated'];
+	public displayedColumns: string[] = ['name', 'symbol', 'slug', 'price', 'percent_change_1h', 'percent_change_24h', 'market_cap', 'last_updated'];
 
 
 	constructor(private config: HttpconfigService) 
@@ -24,14 +24,16 @@ export class HomeComponent implements OnInit {
 	}
 
   // Getting info from server
-	getDevicesInfo(){
+	private getDevicesInfo(): void
+	{
 		this.config.getConfig(this.getConfigUrl)
 			.subscribe(response => {
 				response ? this.coinsInfo.data = response.data.sort((a,b) => b.quote.USD.market_cap - a.quote.USD.market_cap) : this.coinsInfo.data = <any>[];
 			});
 	}
 
-	applyFilter(event: Event) {
+	public applyFilter(event: Event): void
+	{
 		const filterValue = (event.target as HTMLInputElement).value;
 		this.coinsInfo.filter = filterValue.trim().toLowerCase();
 	}
